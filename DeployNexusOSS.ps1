d68d6cf84a51ed8030120deb8d4f2822d6f10a89
Fabian <fabwpp@hotmail.com>
2023-08-07 10:17:17 -0500

    - Added more files and folders to gitignore file - Added new port to Nexus OSS deployment

--- DeployNexusOSS.ps1
+++ DeployNexusOSS.ps1
@@ -1,3 +1,9 @@
 Write-Host "Deploying Nexus OSS"
 
 docker-compose -f .\docker-compose-nexus-oss.yml --env-file .noss-env up -d
+
+Write-Host "Ports running:"
+Write-Host "8081: Web site"
+Write-Host "8082: Docker hub proxy"
+Write-Host "8083: Docker local registry"
+Write-Host "8084: Microsoft registry proxy"
