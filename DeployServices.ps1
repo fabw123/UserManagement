aeee52fe58979d97f4d039fa42927e1a81267c87
Fabian <fabwpp@hotmail.com>
2023-11-29 16:49:55 -0500

    - Updated shell script to use in azure pipeline

--- DeployServices.ps1
+++ DeployServices.ps1
@@ -1,8 +1,8 @@
+Write-Host "Login into Docker Hub"
+docker login --username admin --password Password1234 localhost:8083
+
 Write-Host "Building and tagging Image"
-docker build -t localhost:8083/user_management:latest .
+docker build -t localhost:8083/user_management:latest ./Dockerfile
 
 Write-Host "Pushing image to registry"
 docker push localhost:8083/user_management:latest
-
-Write-Host "Deploying services"
-docker-compose -f .\docker-compose.yml up -d
