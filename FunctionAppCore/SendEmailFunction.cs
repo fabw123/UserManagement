ba6cdd12cc60ed29efdb1573cbe99954c3265cba
Fabian <fabwpp@hotmail.com>
2024-02-28 20:49:23 -0500

    Added SendEmail Azure function

---
+++ FunctionAppCore/SendEmailFunction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace FunctionAppCore
+{
+    public static class SendEmailFunction
+    {
+
+        [FunctionName("SendEmail")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
+        {
+
+            log.LogInformation("Azure Function #2 called successfully");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string email = data?.email;
+
+            string message = $"Azure Function #2 Renewed. Email was sent using the new method to {email}";
+            return new OkObjectResult(message);
+
+        }
+    }
+}
